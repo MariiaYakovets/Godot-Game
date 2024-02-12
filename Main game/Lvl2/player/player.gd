699a902b46abfd4d02702704c46f7b22a8366851
Mariia Yakovets <yamaria05@gmail.com>
2024-02-12 21:16:23 +0000

    updated lvl 2 adding lianas. rocks and missing level pieces

--- Main game/Lvl2/player/player.gd
+++ Main game/Lvl2/player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -340.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,7 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animation_player
 
 func _ready():
-	animation_player = get_node('AnimationPlayer2')
+	animation_player = get_node("AnimationPlayer2")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -26,19 +26,26 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = false
 	elif direction == -1:
 		$AnimatedSprite2D.flip_h = true
+		
 	if direction:
 		velocity.x = direction * SPEED
-		if is_on_floor():
-			animation_player.play('run')
+		if velocity.y == 0:
+			animation_player.play("run")
+			print("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor():
-			animation_player.play('idle')
+		if velocity.y ==0:
+			animation_player.play("idle")
+			print("idle")
+	#if velocity.y > 0:
+	#	animation_player.play("fall")
+	#	print("fall")
+		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		animation_player.play('jump')
+		animation_player.play("jump")
+		print("jump")
 		velocity.y = JUMP_VELOCITY
-	if self.position.y >= 200:
-		queue_free()
+	
 		
 	move_and_slide()
 	
