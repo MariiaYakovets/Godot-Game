1e5f486a75f68a7c9df4dac2949770909dbf76ab
Zixther <duckandfiretto@gmail.com>
2024-03-22 22:38:12 +0200

    Блаженство, даже файлы появились,сделал огонёк как ты и просила P.s Я их по карте расскидал если что
    Signed-off-by: Zixther <duckandfiretto@gmail.com>

--- Main game/Lvl3.1/platforms/moving_platform(up;down).gd
+++ Main game/Lvl3.1/platforms/moving_platform(up;down).gd
@@ -3,8 +3,8 @@ extends Node2D
 var move = 0.0
 
 func _ready() -> void:
-	$Path2D/PathFollow2D/AnimationPlayer.play("moving")
-
+#	$Path2D/PathFollow2D/AnimationPlayer.play("moving")
+	pass
 func _process(delta: float) -> void:
 	move += delta
-	$Path2D/PathFollow2D.progress = move * 100
+	#$Path2D/PathFollow2D.progress = move * 100
