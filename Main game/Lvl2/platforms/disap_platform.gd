6886ed2f8581c86e3ae931983ae8d5cfbbf2f966
Mariia Yakovets <yamaria05@gmail.com>
2024-03-28 11:02:58 +0000

    last camnges

--- Main game/Lvl2/platforms/disap_platform.gd
+++ Main game/Lvl2/platforms/disap_platform.gd
@@ -1,10 +1,8 @@
 extends StaticBody2D
-
-
+var enter: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,6 +11,13 @@ func _process(delta):
 
 func _on_button_for_pl_body_entered(body):
 	if body.is_in_group('player'):
-		$AnimationPlayer.play('disap')
-		await get_node("AnimationPlayer").animation_finished
-		queue_free()
+		if enter:
+			await get_tree().create_timer(5).timeout
+			$AnimationPlayer.play('disap')
+			await get_node("AnimationPlayer").animation_finished
+			
+			queue_free()
+
+
+func _on_start_disappearing_body_entered(body):
+	enter = true
